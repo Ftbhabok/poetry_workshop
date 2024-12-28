9fe2389d33e12b3140a9495623e11bfaf45746f5
ftrav <ftraveller23@gmail.com>
2024-12-28 16:59:14 +0545

    chat area resize

--- src/app/components/ChatInput.tsx
+++ src/app/components/ChatInput.tsx
@@ -1,10 +1,15 @@
 import { cn } from '@/lib/utils'
 import { FC, HTMLAttributes } from 'react'
+import TextareaAutosize from 'react-textarea-autosize'
 
 interface ChatInputProps extends  HTMLAttributes<HTMLDivElement> {}
 
 const ChatInput: FC<ChatInputProps> = ({ className, ...props}) => {
-    return <div {...props} className= {cn('border-t border-zinc-300',className )}>Chatinput</div>
+    return <div {...props} className= {cn('border-t border-zinc-300',className )}>
+        <div className='relative mt-4 flex-1 oveflow-hidden rounded-lg border-none outline-none'>
+       <TextareaAutosize/>
+        </div>
+    </div>
 }
 
 export default ChatInput
